1aef5cdf7af0341d7427954049f143c02b374292
Alex Morega <alex@grep.ro>
2023-03-09 18:26:32 +0200

    Lazy props

--- opslib/props.py
+++ opslib/props.py
@@ -1,12 +1,29 @@
 import beartype.door
 
+from .lazy import evaluate, Lazy
+
 NO_DEFAULT = object()
 
 
 class Prop:
-    def __init__(self, type, default=NO_DEFAULT):
+    def __init__(self, type, default=NO_DEFAULT, lazy=False):
         self.type = type
         self.default = default
+        self.lazy = lazy
+
+    def wrap_lazy(self, name, lazy_value):
+        assert self.lazy
+        assert isinstance(lazy_value, Lazy)
+
+        def get_value_and_check():
+            value = evaluate(lazy_value)
+
+            if not beartype.door.is_bearable(value, self.type):
+                raise TypeError(f"Lazy prop {name!r}: {value!r} is not {self.type!r}")
+
+            return value
+
+        return Lazy(get_value_and_check)
 
 
 class InstanceProps:
@@ -17,15 +34,18 @@ class InstanceProps:
                 continue
 
             value = kwargs.pop(name, prop.default)
+            if prop.lazy and isinstance(value, Lazy):
+                value = prop.wrap_lazy(name, value)
 
-            if value is NO_DEFAULT:
-                if not beartype.door.is_bearable(None, prop.type):
-                    raise TypeError(f"Required prop {name!r} is missing")
+            else:
+                if value is NO_DEFAULT:
+                    if not beartype.door.is_bearable(None, prop.type):
+                        raise TypeError(f"Required prop {name!r} is missing")
 
-                value = None
+                    value = None
 
-            elif not beartype.door.is_bearable(value, prop.type):
-                raise TypeError(f"Prop {name!r}: {value!r} is not {prop.type!r}")
+                elif not beartype.door.is_bearable(value, prop.type):
+                    raise TypeError(f"Prop {name!r}: {value!r} is not {prop.type!r}")
 
             setattr(self, name, value)
 
--- tests/test_props.py
+++ tests/test_props.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import pytest
 
+from opslib.lazy import evaluate, Lazy
 from opslib.props import InstanceProps, Prop
 
 
@@ -10,6 +11,7 @@ class Bench:
         name = Prop(str)
         color = Prop(str, default="black")
         size = Prop(Optional[int])
+        material = Prop(str, default="", lazy=True)
 
     def __init__(self, **kwargs):
         self.props = InstanceProps(type(self), kwargs)
@@ -51,3 +53,21 @@ def test_unexpected():
     assert error.value.args == (
         "'surprise' is an invalid prop for <class 'test_props.Bench'>",
     )
+
+
+def test_lazy_property():
+    bench = Bench(name="bar", material=Lazy(str.title, "wood"))
+    assert evaluate(bench.props.material) == "Wood"
+
+
+def test_lazy_property_not_lazy_value():
+    bench = Bench(name="bar", material="wood")
+    assert evaluate(bench.props.material) == "wood"
+
+
+def test_lazy_property_wrong_type():
+    bench = Bench(name="bar", material=Lazy(lambda: 13))
+    with pytest.raises(TypeError) as error:
+        evaluate(bench.props.material)
+
+    assert error.value.args == ("Lazy prop 'material': 13 is not <class 'str'>",)
