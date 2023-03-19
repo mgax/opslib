9fe66943280b7a30fa629f9677a4f04e722d5534
Alex Morega <alex@grep.ro>
2023-03-19 12:24:26 +0200

    Refactor fixture

--- tests/test_terraform.py
+++ tests/test_terraform.py
@@ -1,68 +1,59 @@
-from pathlib import Path
-
 import pytest
 from click.testing import CliRunner
 
 from opslib.cli import get_main_cli
 from opslib.operations import apply
-from opslib.props import Prop
 from opslib.terraform import TerraformProvider
 from opslib.things import init_statedir
 
 
 @pytest.fixture
-def Bench(Stack):
-    class Bench(Stack):
-        class Props:
-            path = Prop(Path)
+def local_stack(Stack, tmp_path):
+    def create_local_stack(**file_props):
+        path = tmp_path / "hello.txt"
+        file_props.setdefault("type", "local_file")
+        file_props.setdefault("body", dict(content="world", filename=str(path)))
 
-        def build(self):
-            self.provider = TerraformProvider(
-                name="local",
-                source="hashicorp/local",
-                version="~> 2.3",
-            )
+        class LocalStack(Stack):
+            def build(self):
+                self.provider = TerraformProvider(
+                    name="local",
+                    source="hashicorp/local",
+                    version="~> 2.3",
+                )
 
-            self.file = self.provider.resource(
-                type="local_file",
-                body=dict(
-                    content="world",
-                    filename=str(self.props.path),
-                ),
-            )
+                self.path = path
 
-    return Bench
+                self.file = self.provider.resource(**file_props)
+
+        stack = LocalStack()
+        init_statedir(stack)
+        return stack
+
+    return create_local_stack
 
 
-def test_deploy(Bench, tmp_path):
-    path = tmp_path / "hello.txt"
-    stack = Bench(path=path)
-
-    init_statedir(stack)
+def test_deploy(local_stack):
+    stack = local_stack()
     results = apply(stack, deploy=True)
     assert results[stack.file].changed
-    assert path.read_text() == "world"
+    assert stack.path.read_text() == "world"
 
     results = apply(stack, deploy=True)
     assert not results[stack.file].changed
 
 
-def test_cli(Bench, tmp_path, capfd):
-    path = tmp_path / "hello.txt"
-    stack = Bench(path=path)
-    init_statedir(stack)
-    cli = get_main_cli(lambda: stack)
+def test_cli(local_stack, capfd):
+    cli = get_main_cli(local_stack)
     capfd.readouterr()
     CliRunner().invoke(cli, ["file", "terraform", "plan"], catch_exceptions=False)
     captured = capfd.readouterr()
     assert "Terraform will perform the following actions:" in captured.out
 
 
-def test_no_global_plugin_cache(Bench, tmp_path, monkeypatch):
+def test_no_global_plugin_cache(local_stack, monkeypatch):
     monkeypatch.delenv("TF_PLUGIN_CACHE_DIR", raising=False)
-    path = tmp_path / "hello.txt"
-    stack = Bench(path=path)
-    init_statedir(stack)
+    stack = local_stack()
     apply(stack, deploy=True)
 
     [version] = (
