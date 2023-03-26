b6eeb312aa196913f1cc42b5d5ec3e17ca5c64ba
Alex Morega <alex@grep.ro>
2023-03-26 21:09:47 +0300

    Restic

---
+++ opslib/extras/restic.py
@@ -0,0 +1,116 @@
+from io import StringIO
+from shlex import quote
+
+import click
+
+from opslib.components import Component
+from opslib.lazy import Lazy, evaluate, lazy_property
+from opslib.local import run
+from opslib.props import Prop
+from opslib.results import OperationError, Result
+from opslib.state import JsonState
+
+BASH_PREAMBLE = """\
+#!/bin/bash
+set -euo pipefail
+"""
+
+
+class ResticRepository(Component):
+    class Props:
+        repository = Prop(str)
+        password = Prop(str, lazy=True)
+        env = Prop(dict, default={}, lazy=True)
+
+    state = JsonState()
+
+    def refresh(self):
+        try:
+            self.run("list", "index")
+            self.state["initialized"] = True
+
+        except OperationError as error:
+            marker = "Is there a repository at the following location?"
+            if marker not in error.result.output:
+                raise
+
+            self.state["initialized"] = False
+
+        return Result(changed=not self.state["initialized"])
+
+    def deploy(self, dry_run=False):
+        if self.initialized:
+            return Result()
+
+        if dry_run:
+            return Result(changed=True)
+
+        def _run():
+            result = self.run("init", capture_output=False)
+            self.state["initialized"] = True
+            return result
+
+        return Lazy(_run)
+
+    @property
+    def extra_env(self):
+        return dict(
+            RESTIC_REPOSITORY=self.props.repository,
+            RESTIC_PASSWORD=evaluate(self.props.password),
+            **evaluate(self.props.env),
+        )
+
+    def run(self, *args, **kwargs):
+        return run("restic", *args, **kwargs, extra_env=self.extra_env)
+
+    @property
+    def initialized(self):
+        return self.state.get("initialized")
+
+    def plan(self, **props):
+        return ResticPlan(repository=self, **props)
+
+    def add_commands(self, cli):
+        @cli.command(
+            context_settings=dict(
+                ignore_unknown_options=True,
+                allow_interspersed_args=False,
+            )
+        )
+        @click.argument("args", nargs=-1, type=click.UNPROCESSED)
+        def run(args):
+            self.run(*args, capture_output=False, exit=True)
+
+
+class ResticPlan(Component):
+    class Props:
+        repository = Prop(ResticRepository)
+        precommands = Prop(list, default=[])
+        paths = Prop(list, default=[])
+        exclude = Prop(list, default=[])
+        preamble = Prop(str, default=BASH_PREAMBLE)
+
+    @lazy_property
+    def backup_script_content(self):
+        out = StringIO()
+        out.write(self.props.preamble)
+
+        for cmd in self.props.precommands:
+            out.write(f"{cmd}\n")
+
+        for key, value in self.props.repository.extra_env.items():
+            out.write(f"export {key}={quote(value)}\n")
+
+        cmd = ["exec", "restic", "backup"]
+        cmd += [quote(str(path)) for path in self.props.paths]
+        cmd += [f"--exclude={quote(str(path))}" for path in self.props.exclude]
+        out.write(f"{' '.join(cmd)}\n")
+
+        return out.getvalue()
+
+    def backup_script(self, directory, name):
+        return directory.file(
+            name=name,
+            content=self.backup_script_content,
+            mode="700",
+        )
---
+++ tests/test_restic.py
@@ -0,0 +1,65 @@
+import json
+
+import pytest
+
+from opslib.components import init_statedir
+from opslib.extras.restic import ResticRepository
+from opslib.local import run
+from opslib.operations import apply
+from opslib.places import LocalHost
+
+
+@pytest.mark.slow
+def test_initialize(tmp_path, Stack):
+    repo_path = tmp_path / "repo"
+
+    stack = Stack()
+    stack.repo = ResticRepository(
+        repository=str(repo_path),
+        password="not-so-secret",
+    )
+    init_statedir(stack)
+    assert not stack.repo.initialized
+
+    apply(stack, deploy=True)
+    assert repo_path.exists()
+    assert stack.repo.initialized
+
+
+@pytest.mark.slow
+def test_backup_script(tmp_path, Stack):
+    repo_path = tmp_path / "repo"
+    target_path = tmp_path / "target"
+    target_path.mkdir()
+    target_1_path = target_path / "1.txt"
+    target_2_path = target_path / "2.txt"
+    target_1_path.write_text("hello world")
+    backup_script_path = tmp_path / "backup_script"
+
+    stack = Stack()
+    stack.repo = ResticRepository(
+        repository=str(repo_path),
+        password="not-so-secret",
+    )
+    stack.plan = stack.repo.plan(
+        precommands=[f"cp {target_1_path} {target_2_path}"],
+        paths=[target_path],
+        exclude=[target_1_path],
+    )
+    stack.script = stack.plan.backup_script(
+        directory=LocalHost().directory(backup_script_path.parent),
+        name=backup_script_path.name,
+    )
+    init_statedir(stack)
+    assert not stack.repo.initialized
+
+    apply(stack, deploy=True)
+    run(backup_script_path)
+
+    snapshot_paths = {
+        json.loads(line).get("path")
+        for line in stack.repo.run("ls", "latest", "--json").stdout.splitlines()
+    }
+    assert str(target_1_path) not in snapshot_paths
+    assert str(target_2_path) in snapshot_paths
+    assert stack.repo.run("dump", "latest", target_2_path).stdout == "hello world"
