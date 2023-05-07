b593d6745a8b22ec9616bed58a32258b458f6afb
Alex Morega <alex@grep.ro>
2023-05-07 11:07:28 +0300

    Convenience imports

---
+++ opslib/__init__.py
@@ -0,0 +1,6 @@
+from .components import Component, Stack
+from .lazy import Lazy, evaluate, lazy_property
+from .local import run
+from .places import Command, Directory, File, LocalHost, SshHost
+from .props import Prop
+from .state import JsonState
