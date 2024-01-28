f8b2346885591d1b91bf359eccf9655314d41603
Alex Morega <alex@grep.ro>
2024-01-28 15:57:10 +0200

    Switch to Flit

--- opslib/__init__.py
+++ opslib/__init__.py
@@ -1,6 +1,27 @@
+"""A Pythonic toolkit to manage infrastructure."""
+
 from .components import Component, Stack
 from .lazy import Lazy, MaybeLazy, evaluate, lazy_property
 from .local import run
 from .places import Command, Directory, File, LocalHost, SshHost
 from .props import Prop
 from .state import JsonState
+
+__all__ = [
+    "Command",
+    "Component",
+    "Directory",
+    "File",
+    "JsonState",
+    "Lazy",
+    "LocalHost",
+    "MaybeLazy",
+    "Prop",
+    "SshHost",
+    "Stack",
+    "evaluate",
+    "lazy_property",
+    "run",
+]
+
+__version__ = "0.1"
